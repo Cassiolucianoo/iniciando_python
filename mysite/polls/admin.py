e19d922780cf91edee08c7900daaf76f5a9ea3d3
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2021-06-16 23:52:16 -0300

    search_fields = __exact

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-
 # Register your models here.
 from .models import Question
+from django.db.models import Q
+from functools import reduce
+import unidecode
 
-admin.site.register(Question)
+
+
+
+class ListandoPerguntas(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+    list_display_link = ('question_text',)
+    search_fields = ('question_text__exact',)
+
+    """ def get_search_results(self, request, queryset, search_term):
+        queryset, may_have_duplicates = super().get_search_results(
+            request, queryset, search_term,
+        )
+        
+        search_term_as_int = int(search_term)
+    
+        queryset |= self.model.objects.filter(age=search_term_as_int)
+        return queryset, may_have_duplicates
+ """
+      
+	
+
+admin.site.register(Question, ListandoPerguntas)
